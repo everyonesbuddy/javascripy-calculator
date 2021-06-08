5d3b3990a57cb22609bab4cf88efab8ec8e11b90
Joseph Adeyemo <j_adeyemo@yahoo.com>
2021-06-08 12:49:32 -0400

    6/8/2021 12:49PM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [expression, setExpression] = useState("")
+
+  const display = (symbol) => {
+    setExpression(prev => prev + symbol)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="grid">
+        <div className="dis">
+
+        </div>
+        <div onClick = {display} className="padButton AC tomato">AC</div>
+        <div onClick = {display} className="padButton C tomato">C</div>
+        <div onClick = {display} className="padButton div">/</div>
+        <div onClick = {display} className="padButton times">*</div>
+        <div onClick = {display} className="padButton seven dark-gray">7</div>
+        <div onClick = {display} className="padButton eight dark-gray">8</div>
+        <div onClick = {display} className="padButton nine dark-gray">9</div>
+        <div onClick = {display} className="padButton minus">-</div>
+        <div onClick = {display} className="padButton four dark-gray">4</div>
+        <div onClick = {display} className="padButton five dark-gray">5</div>
+        <div onClick = {display} className="padButton six dark-gray">6</div>
+        <div onClick = {display} className="padButton plus">+</div>
+        <div onClick = {display} className="padButton one dark-gray">1</div>
+        <div onClick = {display} className="padButton two dark-gray">2</div>
+        <div onClick = {display} className="padButton three dark-gray">3</div>
+        <div onClick = {display} className="padButton equal blue">=</div>
+        <div onClick = {display} className="padButton zero dark-gray">0</div>
+        <div onClick = {display} className="padButton dot dark-gray">.</div>
+      </div>
+
     </div>
   );
 }
